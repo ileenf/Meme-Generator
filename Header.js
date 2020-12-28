50d2df151ea28ca17fb9e0b830a46d0a11e834bf
ileenf <52259696+ileenf@users.noreply.github.com>
2020-12-28 00:00:34 -0800

    adding js files

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return(
+        <header>
+            <img src = "https://media.tenor.com/images/07897c06242f1329ef7c37878b6bed94/tenor.png" alt = "Pikachu" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
